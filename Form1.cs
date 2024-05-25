c2c026a3b5c03065c68c65e9427f48793e6b5a79
xHuGODx <hugo.ribeiro04@ua.pt>
2024-05-25 10:22:28 +0100

    mudei o loadteams para usar uma view

--- Form1.cs
+++ Form1.cs
@@ -56,12 +56,7 @@ namespace ValoLeague
 
             try
             {
-                string query = @"
-                    SELECT 
-                        t.Team_ID, t.Nome, t.Foundation_Date, 
-                        (SELECT COUNT(*) FROM VALO_PLAYER p WHERE p.Team_ID = t.Team_ID) AS PlayerCount,
-                        (SELECT COUNT(*) FROM VALO_COACH c WHERE c.Team_ID = t.Team_ID) AS CoachCount
-                    FROM VALO_TEAM t";
+                string query = "SELECT * FROM ListTeamsView";
 
                 adapter = new SqlDataAdapter(query, cn);
                 dataSet = new DataSet();
